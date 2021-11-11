b4bc134c743988d0a21685f2afd4af83ec1b2a78
josueneneve <josue.neneve@hotmail.com>
2021-11-11 08:06:38 -0300

    Payment, one-to-one association

--- src/main/java/com/josueneneve/webservices/entities/OrderItem.java
+++ src/main/java/com/josueneneve/webservices/entities/OrderItem.java
@@ -12,16 +12,17 @@ import com.josueneneve.webservices.entities.pk.OrderItemPK;
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
-	public OrderItem() {}
+
+	public OrderItem() {
+	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
 		super();
@@ -30,20 +31,20 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
